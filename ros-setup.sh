2d0579139849b53d0733a0ead99c10b9c1251e3e
Aswin Sarang <ashsvj@gmail.com>
2017-03-29 11:50:25 +0400

    Add files via upload

---
+++ ros-setup.sh
@@ -0,0 +1,11 @@
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo apt-get update
+echo "\033[1;97mInstalling ROS\033[0m"
+sudo apt-get install ros-kinetic-desktop-full
+apt-cache search ros-kinetic
+sudo rosdep init
+rosdep update
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+sudo apt-get install python-rosinstall
