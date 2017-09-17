8e5922d3a000c0002ef8aaa2e351fe465135fdfc
Aswin Sarang <ashsvj@gmail.com>
2017-09-17 09:32:17 +0400

    Update and rename ros-setup.sh to ros_indigo_setup.sh

--- ros_indigo_setup.sh
+++ ros_indigo_setup.sh
@@ -1,12 +1,19 @@
+#!/bin/bash
+#Copy this file to Home directory
+#Open terminal and type the following commands
+#chmod + ros_indigo_install.sh
+#./ros_indigo_install
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt-get update
-echo "\033[1;97mInstalling ROS\033[0m"
-sudo apt-get install ros-kinetic-desktop-full
-apt-cache search ros-kinetic
+sudo apt-get install ros-indigo-desktop-full
 sudo rosdep init
 rosdep update
-echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 sudo apt-get install python-rosinstall
-sudo apt-get install ros-kinetic-jackal-simulator ros-kinetic-jackal-desktop
+mkdir -p ~/catkin_ws/src
+cd catking_ws/src
+catkin_init_workspace
+cd ..
+catkin_make
